f7419dd2f95c9a82fdbb83c83fbb06b4e56d854d
Yeounoh Chung <yeounoh@google.com>
2023-11-14 15:32:31 -0800

    Add ShadingUtil::ReshardParameters

--- torch_xla/csrc/xla_sharding_util.h
+++ torch_xla/csrc/xla_sharding_util.h
@@ -3,6 +3,8 @@
 
 #include <torch/csrc/jit/python/pybind.h>
 
+#include <tuple>
+
 #include "torch_xla/csrc/ir.h"
 #include "torch_xla/csrc/lowering_context.h"
 #include "torch_xla/csrc/runtime/computation_client.h"
@@ -95,8 +97,8 @@ class ShardingUtil {
       const at::Tensor& tensor, const XLATensor::ShardingSpecPtr shardings,
       const std::vector<std::string>& devices, bool padded = true);
 
-  // Retrieve output sharding of a given XLA computation. ShardingSpe::shape is
-  // always on virtual SPMD device.
+  // Retrieve output sharding of a given XLA computation. ShardingSpe::shape
+  // is always on virtual SPMD device.
   static std::vector<XLATensor::ShardingSpecPtr> GetOutputSharding(
       const std::vector<xla::Shape>& output_shapes,
       runtime::ComputationClient::ComputationPtr computation);
@@ -125,11 +127,29 @@ class ShardingUtil {
       const std::vector<std::string>& devices,
       const XLATensor::ShardingSpecPtr& sharding_spec);
 
-  static void XlaMarkSharding(const at::Tensor& input,
-                              xla::OpSharding sharding);
+  //////////////////////////// Auto-Sharding ////////////////////////////
+
+  // Construct a device mesh for auto-sharding pass. Returns a tuple of mesh
+  // shape and device ids vectors.
+  // TODO(yeounoh) integrate with automatic mesh selector.
+  static std::tuple<std::vector<int64_t>, std::vector<int64_t>>
+  GetAutoShardingMesh();
+
+  // Reshard the parameters if the expected shardings mismatch. Resharding is
+  // expensive especially for those already sharded. The cost can easily be
+  // armotized over multiple steps, though, since the input sharding is
+  // propagated to the output for the subsequent runs. Sharded data transfer
+  // during resharding should be asynchronous. It is recommended to keep the
+  // input sharding on the input data as-is. Return true if resharded any of
+  // the parameters.
+  static bool ReshardParameters(
+      const xla::HloModuleProto& module,
+      std::vector<torch::lazy::BackendDataPtr>* parameters);
 
   //////////////////////////// Dynamo Integration ////////////////////////////
 
+  static void XlaMarkSharding(const at::Tensor& input,
+                              xla::OpSharding sharding);
   static void XlaMarkShardingDynamoCustomOp(
       const at::Tensor& input, c10::List<at::IntArrayRef> tile_assignment,
       c10::List<at::IntArrayRef> group_assignment,
