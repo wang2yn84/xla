0c9109572e1be78756faf040013450639396adb1
Greg Olechwierowicz <olechwierowicz@google.com>
2024-02-16 17:22:00 +0100

    Add XLA specific XPlane profiling.

--- benchmarks/check_xla_device.py
+++ benchmarks/check_xla_device.py
@@ -3,9 +3,11 @@ assert len(sys.argv) == 2
 devkind = sys.argv[1]
 
 import os
+
 os.environ["PJRT_DEVICE"] = devkind
 
 import torch_xla.core.xla_model as xm
+
 devlist = xm.get_xla_supported_devices(devkind=devkind)
 
 if devlist is None or len(devlist) == 0:
